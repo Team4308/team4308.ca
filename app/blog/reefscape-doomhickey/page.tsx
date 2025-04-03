e92220e15a56323ee173cb03d8bf346956e6c0bc
Benjamin Lu <templu1107@proton.me>
2025-04-02 20:06:14 -0400

    Add detailed description of Doomhickey robot for 2025 season

--- app/blog/reefscape-doomhickey/page.tsx
+++ app/blog/reefscape-doomhickey/page.tsx
@@ -20,6 +20,19 @@ export default function Sponsor() {
         <p className="mx-auto max-w-xl text-[#f1f7feb5]">
           We&apos;re proud to present our 2025 robot for the Reefscape season -
           Doomhickey!
+          <br />
+          <br />
+          Doomhickey is an agile scoring robot that excels at both L3 and L4
+          tasks while also knocking down algae during operation.
+          <br />
+          <br />
+          Its scoring efficiency is boosted by a high-speed swerve drive that
+          achieves cycle times in under 4 seconds, and its performance
+          consistency is maintained with an integrated autoaligner.
+          <br />
+          <br />
+          We're excited to see how Doomhickey performs in the 2025 season, and
+          we hope to see you at our competitions!
         </p>
       </div>
     </div>
