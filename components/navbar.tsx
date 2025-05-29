cfc185530877ee18da5699de65059d84616bfcda
Dalton Su <dalton.jin.su@gmail.com>
2025-05-29 09:28:39 -0400

    animations for navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { ChevronDownIcon, HamburgerMenuIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 import { NavigationMenu } from "radix-ui";
 import { useEffect, useState } from "react";
@@ -14,29 +14,35 @@ export default function NavBar() {
   })
 
   return (
-    <NavigationMenu.Root className="flex flex-row fixed top-0 w-screen px-6 lg:px-[20%] py-3 md:py-5 items-center">
-      <Image src="/logo.png" alt="logo" width={128} height={128} className="mr-auto size-10"/>
-      <div className={`md:hidden size-8 ${nav ? "z-0" : "z-1"}`} onClick={()=>setNav(true)}><HamburgerMenuIcon className="size-full"/></div>
+    <NavigationMenu.Root className="fixed flex flex-row top-0 w-screen px-6 lg:px-[20%] py-3 items-center">
+      <Logo className="mr-auto ml-2"/>
+      <div className={`md:hidden size-9 ${nav ? "z-0" : "z-1"}`} onClick={()=>setNav(true)}><HamburgerMenuIcon className="size-full"/></div>
 
-      <div className={`fixed flex md:hidden top-0 left-0 w-screen h-screen backdrop-blur-xs ${nav ? "opacity-100" : "opacity-0"} ease-in-out duration-300`} onClick={()=>setNav(false)}/>
+      <div className={`fixed flex md:hidden top-0 left-0 w-screen h-screen bg-black ${nav ? "opacity-30" : "opacity-0"} ${transition}`} onClick={()=>setNav(false)}/>
 
       <NavigationMenu.List 
-        className={`${nav ? "left-0" : "left-[-65%]"} fixed md:static top-0 flex flex-col md:flex-row gap-3 md:gap-6 w-[65%] md:w-auto h-full 
-                    md:items-center bg-foreground px-10 py-3 md:p-0 ease-in-out duration-300 md:transition-none`}
+        className={`${nav ? "left-0" : "left-[-65%]"} fixed md:static top-0 flex flex-col md:flex-row gap-4 md:gap-1 w-[65%] md:w-auto h-full 
+                    md:items-center bg-foreground px-10 py-3 md:p-0 ${transition} md:transition-none`}
       >
-        <Image src="/logo.png" alt="logo" width={128} height={128} className="mb-6 size-10 md:hidden"/>
+        <Logo className="mb-6 md:hidden"/>
 
         <SimpleItem href="/" label="Home"/>
-        <SimpleItem href="/about" label="About Us"/>
+        <Seperator/>
 
-        <ComplexItem label="Outreach" items={[
+        <SimpleItem href="/about" label="About Us"/>
+        <Seperator/>
+
+        <ComplexItem label="Outreach" hrefBase="/outreach" items={[
           { href: "/fll", label: "FLL" },
           { href: "/tree-planting", label: "Tree Planting" },
         ]}/>
-        <ComplexItem label="Resources" items={[
+        <Seperator/>
+
+        <ComplexItem label="Resources" hrefBase="/resources" items={[
           { href: "/docs", label: "Documentation" },
           { href: "/blogs", label: "Blogs" },
         ]}/>
+        <Seperator/>
 
         <SimpleItem href="/sponsors" label="Sponsors"/>
       </NavigationMenu.List>
@@ -44,27 +50,42 @@ export default function NavBar() {
   );
 }
 
+function Logo({className}: {className: string | undefined}) {
+  return (
+    <Image src="/logo.png" alt="logo" width={128} height={128} className={`size-10 ${className}`}/>
+  );
+}
+
+function Seperator() {
+  return ( <div className="w-full h-px md:w-px md:h-9 bg-background"/> );
+}
+
 function SimpleItem({href, label}: {href: string, label: string}) {
   return (
     <NavigationMenu.Item>
-      <NavigationMenu.Link href={href}>{label}</NavigationMenu.Link>
+      <NavigationMenu.Link href={href} className={`trigger ${trigger}`}>{label}</NavigationMenu.Link>
     </NavigationMenu.Item>
   );
 }
 
-function ComplexItem({label, items}: {label: string, items: {href: string, label: string}[]}) {
+function ComplexItem({label, hrefBase, items}: {label: string, hrefBase: string, items: {href: string, label: string}[]}) {
   return (
     <NavigationMenu.Item className="relative">
-      <NavigationMenu.Trigger>{label}</NavigationMenu.Trigger>
-      <NavigationMenu.Content className="flex md:text-center text-nowrap md:absolute md:top-[100%] md:left-[50%] md:translate-x-[-50%] px-4 md:py-3 bg-foreground">
-        <ul className="flex flex-col md:items-center">
+      <NavigationMenu.Trigger className={`trigger ${trigger} flex flex-row`}>{label}<ChevronDownIcon className={`caret transition-transform ${transition} p-[-25%] static size-4 self-center`}/></NavigationMenu.Trigger>
+      <NavigationMenu.Content className="content flex md:text-center text-nowrap md:absolute md:top-[100%] md:left-[50%] md:translate-x-[-50%] px-6 md:p-0 md:bg-background">
+        <ul className="flex flex-col md:items-center gap-2 md:gap-px">
+          <div className="md:hidden h-0"/>
           {items.map(({href, label}) => {
             return (
-              <NavigationMenu.Link key={label} href={href}>{label}</NavigationMenu.Link>
+              <NavigationMenu.Link key={label} href={hrefBase + href} className={`trigger w-full md:px-5 md:py-2 md:bg-nav-dropdown`}>{label}</NavigationMenu.Link>
             );
           })}
+          <div className="md:hidden h-2"/>
         </ul>
       </NavigationMenu.Content>
     </NavigationMenu.Item>
   );
 }
+
+const transition = "ease-in-out duration-300";
+const trigger = `md:p-2 ${transition}`;
