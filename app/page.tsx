bf73c4e65cb0bd33eac6ed602fd76fb76a82e20f
Dalton Su <dalton.jin.su@gmail.com>
2025-12-20 20:24:19 -0500

    updated home page + some shared components

--- app/page.tsx
+++ app/page.tsx
@@ -12,21 +12,25 @@ export default function Home() {
         desc="A FIRST robotics team from Mississauga, Ontario"
       />
 
-      <h2 className="text-center text-4xl font-medium mt-12">Some more photos of us and our bots</h2>
+      <h2 className="text-center max-sm:text-2xl sm:text-3xl md:text-4xl font-medium mt-12 px-5">More photos of us and our bots</h2>
 
       <CustomCarousel
-        childClass="w-120 h-80"
-        className="mt-2"
+        childClass="mx-[10%] aspect-3/2"
         src="/carousel-images"
         responsive={{
-          all: {
-            breakpoint: { max: 100000, min: 0 },
+          allOthers: {
+            breakpoint: { max: 100000, min: 641 },
             items: 2
+          },
+          mobile: {
+            breakpoint: { max: 640, min: 0 },
+            items: 1
           }
         }}
+        className="pt-2"
         arrows={false}
         autoPlay
-        autoPlaySpeed={3000}
+        autoPlaySpeed={4000}
         customTransition="transform 800ms ease-in-out"
         transitionDuration={800}
         infinite
@@ -34,7 +38,7 @@ export default function Home() {
 
       <EventCountdown />
 
-      <h3 className="text-center text-2xl mt-20 mb-3">Generously sponsored by:</h3>
+      <h3 className="text-center text-3xl max-sm:text-2xl mt-20 mb-3">Generously sponsored by:</h3>
 
       <SponsorsCarousel className="mb-10" />
     </>
