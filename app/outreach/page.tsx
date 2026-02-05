db59f62ce10256ef02e89662fb587c11d7e9132c
Benjamin <benwu2017g2@gmail.com>
2026-02-05 15:21:01 -0500

    made sponsorship page work on small screen devices

--- app/outreach/page.tsx
+++ app/outreach/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import HeroBanner from "@/components/hero-banner";
 import Image from "next/image";
@@ -6,39 +6,49 @@ import Link from "next/link";
 import React, { ReactNode } from "react";
 import { useState, useEffect, useRef } from "react";
 
-function Featured({ href, title, imgSrc }: { href: string, title: string, imgSrc: string }) {
+function Featured({
+  href,
+  title,
+  imgSrc,
+}: {
+  href: string;
+  title: string;
+  imgSrc: string;
+}) {
   return (
     <div className="flex flex-col">
-      <p className="text-2xl text-center">{title}</p>
+      <p className="text-center text-2xl">{title}</p>
       <a href={href}>
-        <div className="relative w-[200] h-[200px] overflow-hidden rounded-lg">
-          <Image
-            src={imgSrc}
-            alt=""
-            fill
-            className="object-cover"
-          ></Image>
+        <div className="relative h-[200px] w-[200] overflow-hidden rounded-lg">
+          <Image src={imgSrc} alt="" fill className="object-cover"></Image>
         </div>
       </a>
     </div>
   );
 }
 
-function Thing({ title, imgSrc, imgPos, children }: { imgSrc: string, imgPos: "left" | "right", title: string, children: ReactNode }) {
+function Thing({
+  title,
+  imgSrc,
+  imgPos,
+  children,
+}: {
+  imgSrc: string;
+  imgPos: "left" | "right";
+  title: string;
+  children: ReactNode;
+}) {
   return (
-    <div className="w-full flex justify-center mt-16">
-      <div className={`flex items-start gap-4 pb-4 w-4xl ${imgPos == "right" ? "flex-row-reverse" : ""}`}>
+    <div className="mt-16 flex justify-center">
+      <div
+        className={`flex w-4xl items-start gap-4 px-3 pb-4 lg:px-0 ${imgPos == "right" ? "flex-row-reverse" : ""}`}
+      >
         <div>
-          <div className="relative w-100 h-125 overflow-hidden rounded-lg">
-            <Image
-              src={imgSrc}
-              alt=""
-              fill
-              className="object-cover"
-            ></Image>
+          <div className="relative overflow-hidden rounded-lg md:h-125 md:w-100">
+            <Image src={imgSrc} alt="" fill className="object-cover"></Image>
           </div>
         </div>
-        <div className="text-lg flex flex-col items-start justify-start pt-2 my-auto gap-5">
+        <div className="my-auto flex flex-col items-start justify-start gap-5 pt-2 text-lg">
           <h1 className="text-5xl">{title}</h1>
           {children}
         </div>
@@ -51,7 +61,11 @@ export default function Outreach() {
   const [carbon, setCarbon] = useState(0);
   const [trees, setTrees] = useState(0);
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const counter = (setter: (n: number) => void, max: number, duration: number) => {
+  const counter = (
+    setter: (n: number) => void,
+    max: number,
+    duration: number
+  ) => {
     let start = 0;
     const stepTime = 16;
     const totalSteps = duration / stepTime;
@@ -69,7 +83,12 @@ export default function Outreach() {
   };
   useEffect(() => {
     const curr = new Date();
-    const diff = (curr.getTime() - new Date("May 27, 2025").getTime()) / 1000 / 60 / 60 / 24;
+    const diff =
+      (curr.getTime() - new Date("May 27, 2025").getTime()) /
+      1000 /
+      60 /
+      60 /
+      24;
     const observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
@@ -86,34 +105,39 @@ export default function Outreach() {
   }, []);
   return (
     <div>
-      <HeroBanner
-        src="big-image.jpg"
-        title="Outreach"
-      />
+      <HeroBanner src="big-image.jpg" title="Outreach" />
 
-      <Thing imgPos="right" title="4308 tree planting" imgSrc="/res/blog/tree-planting/tree-planting-carousel/tree-planting (2).jpg">
+      <Thing
+        imgPos="right"
+        title="4308 tree planting"
+        imgSrc="/res/blog/tree-planting/tree-planting-carousel/tree-planting (2).jpg"
+      >
         <p>
-          In May of 2025, team 4308 collaborated with the
-          City of Mississauga to plant trees in the
-          Woodlands Meadow Park.
+          In May of 2025, team 4308 collaborated with the City of Mississauga to
+          plant trees in the Woodlands Meadow Park.
         </p>
         <p>
-          Around 15 volunteers and team members spent 3 hours
-          their afternoon planting a total of 200 trees!
+          Around 15 volunteers and team members spent 3 hours their afternoon
+          planting a total of 200 trees!
         </p>
         <p>
-          The trees that we planted offsets around 12kg of
-          carbon dioxide every single day!
+          The trees that we planted offsets around 12kg of carbon dioxide every
+          single day!
         </p>
       </Thing>
-      <div className="content-center mt-20 pt-10 mb-5 pb-15 bg-gray-200" ref={containerRef}>
-        <h1 className="text-6xl text-center mt-5 mb-7.5 font-bold">4308 Outreach Has</h1>
-        <div className="text-5xl leading-none grid grid-cols-2 gap-x-30 text-center">
-          <div className="font-semibold flex flex-col items-end space-y-3">
+      <div
+        className="mt-20 mb-5 content-center bg-gray-200 pt-10 pb-15"
+        ref={containerRef}
+      >
+        <h1 className="mt-5 mb-7.5 text-center text-6xl font-bold">
+          4308 Outreach Has
+        </h1>
+        <div className="grid grid-cols-2 gap-x-5 text-center text-3xl leading-none md:gap-x-30 md:text-5xl">
+          <div className="flex flex-col space-y-3 font-semibold md:items-end">
             <h1>Planted</h1>
             <h1>Carbon offset</h1>
           </div>
-          <div className="flex flex-col items-start space-y-3">
+          <div className="flex flex-col items-start space-y-6 md:space-y-3">
             <p>{trees}+ Trees</p>
             <p>{carbon}+ Kgs</p>
           </div>
@@ -121,26 +145,31 @@ export default function Outreach() {
       </div>
       <Thing imgPos="left" title="hack:peel" imgSrc="/big-image.jpg">
         <p>
-          Team 4308 is collaborating with the Woodlands computer science club
-          to bring you hack::peel 2026! hack::peel is a 24-hour hackathon
-          that gives students real experience and the opportunity to build
-          something great!
+          Team 4308 is collaborating with the Woodlands computer science club to
+          bring you hack::peel 2026! hack::peel is a 24-hour hackathon that
+          gives students real experience and the opportunity to build something
+          great!
         </p>
         <p>
           The last time hack::peel was run, we attracted more than 100
           participants. We gave nearly $15,000 worth of prizes.
         </p>
         <p>
-          This year, hack::peel is planning to run again in late May.
-          Make sure to check
-          out <Link href="" target="_blank" className="text-link hover:text-link-hover">hack-peel.</Link> for
-          more details!
+          This year, hack::peel is planning to run again in late May. Make sure
+          to check out{" "}
+          <Link
+            href=""
+            target="_blank"
+            className="text-link hover:text-link-hover"
+          >
+            hack-peel.
+          </Link>{" "}
+          for more details!
         </p>
       </Thing>
-
-      <div className="justify-center mt-16 mb-10">
-        <h1 className="text-6xl text-center">Featured on:</h1>
-        <div className="flex justify-center gap-5 pt-4 pb-8">
+      <h1 className="text-center text-6xl">Featured on:</h1>
+      <div className="md:overflow-none mt-16 mb-10 max-w-full justify-center overflow-auto">
+        <div className="flex gap-5 pt-4 pb-8 md:justify-center">
           <Featured
             title="Humber"
             href="https://humbernews.ca/2023/03/absolute-robotics-won-bronze-in-first-robotics-despite-setbacks/"
